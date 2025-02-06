6c1f22af2e06da9780b3566ea206667ad71e0104
HMDSS <halilmertdeveliii@gmail.com>
2025-02-06 14:13:59 +0300

    first

---
+++ MurderGame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MurderGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
