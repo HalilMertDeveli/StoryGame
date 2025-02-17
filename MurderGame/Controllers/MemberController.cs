643eb11a3d93da3bec5c7a0d994aef174adb1216
HMDSS <halilmertdeveliii@gmail.com>
2025-02-17 15:22:53 +0300

    logins not working

--- MurderGame/Controllers/MemberController.cs
+++ MurderGame/Controllers/MemberController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MurderGame.UI.Controllers
 {
+    [Authorize(Roles = "Member")]
+
     public class MemberController : Controller
     {
         public IActionResult Index()
